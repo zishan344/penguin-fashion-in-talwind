bc2a23e0ad70fde51b571dcd55f7f605e50daee8
zishan344 <zishanahmed344@gmail.com>
2022-02-12 11:42:48 +0600

    added javascript

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById("special-btn").addEventListener("click", function () {
+  const specialText = document.getElementById("special-text");
+  specialText.innerText = "I AM PENGUIN";
+});
